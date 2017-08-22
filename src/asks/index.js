156a6b1f95dd8323e6a29d1f6dbf80255791c733
JR Palomar <jr@macremefraiche.com>
2017-08-21 23:07:30 -0400

    whitespace;

--- src/asks/index.js
+++ src/asks/index.js
@@ -194,7 +194,7 @@ class AsksView {
 =========================== */
 
 // instantiation
-const view = new AsksView('.question-item')
+const view = new AsksView( '.question-item' )
 
 view.fetch_then_render( 'https://nfte-2017-writeme.github.io/writeme/mocks/endpoints/qna/index.json' )
 /* =====  End of run  ====== */
