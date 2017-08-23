84e4d0a362dec0e8c153a5c395afc08a33c31b12
Mehdad Zaman <zaman.mehdad.227@gmail.com>
2017-08-23 11:23:25 -0400

    lki

--- src/Prompts/prompts.js
+++ src/Prompts/prompts.js
@@ -15,11 +15,8 @@ function load() {
 	var mydata = JSON.parse(data);
 	document.write(mydata(num).question);
 }
-
-
 var z = Object.keys(data)
 alert(i);
-
 for i in len(mydata):
     if i == "question":
         something+=1
