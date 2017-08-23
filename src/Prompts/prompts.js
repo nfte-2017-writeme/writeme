244bff9c4413bb7dc471704eaab7e0e257b9d922
Alex McGuigan <amcguiga@gmail.com>
2017-08-22 20:32:06 -0400

    random prompt generator

--- src/Prompts/prompts.js
+++ src/Prompts/prompts.js
@@ -1,12 +1,26 @@
+function displayOne() {
+	var data = JSON.parse(fetch("https://nfte-2017-writeme.github.io/writeme/mocks/endpoints/qna/index.json").response);
+
+	console.log(data[0].question);
+
+	var num = Math.floor(Math.random() * (data.length - 1));
+
+	console.log(num);
+
+	document.write(data[num].question);
+}
+
+/*
 function load() {
 	var mydata = JSON.parse(data);
 	document.write(mydata(num).question);
 }
 
-var num = Math.floor((Math.random() * 2) + 1);
+
 var z = Object.keys(data)
 alert(i);
 
 for i in len(mydata):
     if i == "question":
         something+=1
+*/
