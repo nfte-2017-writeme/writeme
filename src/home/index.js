44141e6399d8c8f36952ca1b15c10eab31ccc947
JR Palomar <jr@macremefraiche.com>
2017-09-14 07:46:49 -0400

    improve rendering states when swiping

--- src/home/index.js
+++ src/home/index.js
@@ -1,27 +1,35 @@
 ! function () {
-    /* ==========================    =
-    =========================== */
+    /* =====  run  ====== */
 
     // instantiation
     const view = new AsksView( '.question-item' );
     const stack = Swing.Stack();
+    const render_inactive_state = ( list ) => {
+        list.remove( 'btn-info' );
+        list.add( 'btn-dark' );
+    };
+    const render_active_state = ( list, state = 'info' ) => {
+        list.remove( 'btn-dark' );
+        list.remove( 'btn-info' );
+        list.add( `btn-${state}` );
+    };
 
     // Events
-    stack.on( 'dragstart', ( e ) => {
-        e.target.classList.add( 'btn-info' );
+    stack.on( 'dragstart', ( evt ) => {
+        render_active_state( evt.target.classList );
     } );
 
-    stack.on( 'dragend', ( e ) => {
-        e.target.classList.remove( 'btn-info' );
+    stack.on( 'throwin', ( evt ) => {
+        render_inactive_state( e.target.classList );
     } );
 
-    stack.on( 'throwoutleft', ( e ) => {
-        e.target.classList.add( 'btn-danger' );
+    stack.on( 'throwoutleft', ( evt ) => {
+        render_active_state( evt.target.classList, 'danger' );
         window.setTimeout( () => window.location.reload(), 500 );
     } );
 
-    stack.on( 'throwoutright', ( e ) => {
-        e.target.classList.add( 'btn-success' );
+    stack.on( 'throwoutright', ( evt ) => {
+        render_active_state( evt.target.classList, 'success' );
         window.setTimeout( () => window.location.assign( '../loading' ), 750 );
     } );
 
